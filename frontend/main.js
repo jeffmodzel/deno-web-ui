d50665c15cd8251a85f6bca741f5628e9ec86ad6
Jeffrey Modzel <jeffrey.modzel@ttecdigital.com>
2025-10-10 07:44:17 -0400

    pre work commit

--- frontend/main.js
+++
@@ -1,5 +0,0 @@
-// ../lib/mod.ts
-var APP_TITLE = "APP_TITLE change me";
-
-// main.ts
-document.title = APP_TITLE;
--- frontend/main.ts
+++ frontend/main.ts
@@ -1,10 +1,26 @@
-
 /// <reference lib="dom" />
-
+import Alpine from 'alpinejs'
 import { APP_TITLE } from '@workspace/lib';
+
+//
+// This is apparently not required, even though Alpine doc says it is
+//
+// The declare global block tells TypeScript that you're extending existing global types, and the interface Window declaration merges with the existing Window interface to add the Alpine property.
+// declare global {
+//     interface Window {
+//         Alpine: typeof Alpine;
+//     }
+// }
+//window.Alpine = Alpine
+//Alpine.start()
 
 // Set the browser title when the script loads
 document.title = APP_TITLE;
 
+const store = {
+    display: 'value from alpine store',
+};
+Alpine.store('clock', store);
 
 
+Alpine.start()
